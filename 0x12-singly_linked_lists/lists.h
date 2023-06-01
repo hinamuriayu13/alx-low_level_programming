29fdc68844f17541801ae91fb3f8f28945b14c89
hinamuriayu13 <harukanyan1@gmail.com>
2023-06-01 11:55:43 -0700

    task1

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,10 +4,10 @@
 #include <stddef.h>
 
 /**
- * struct list_s : 'singly linked list'
- * @str= string : (memory allocated for the string with 'malloc')
- * @len= length of the string
- * @next= pointer to the next node
+ * struct list_s - 'singly linked list'
+ * @str: string - (memory allocated for the string with 'malloc')
+ * @len: length of the string
+ * @next: pointer to the next node
  *
  * Description: Singly linked list node structure
  *              for a Holberton project
@@ -20,42 +20,42 @@ typedef struct list_s
 } list_t;
 
 /**
- * print_list : Prints all the elements of a linked list
- * @h= Pointer to the head node of the list
+ * print_list - Prints all the elements of a linked list
+ * @h: Pointer to the head node of the list
  *
- * Return= The number of nodes in the list
+ * Return: The number of nodes in the list
  */
 size_t print_list(const list_t *h);
 
 /**
- * list_len : Counts the number of nodes in a linked list
- * @h= Pointer to the head node of the list
+ * list_len - returns the number of nodes in the  linked list
+ * @h: Pointer to the head node of the list
  *
- * Return= The number of nodes in the list
+ * Return: The number of nodes in the list
  */
 size_t list_len(const list_t *h);
 
 /**
- * add_node : Adds a new node at the beginning of a linked list
- * @head= Pointer to the pointer to the head node of the list
- * @str= The string to be added to the new node
+ * add_node - Adds a new node at the beginning of a linked list
+ * @head: Pointer to the pointer to the head node of the list
+ * @str: The string to be added to the new node
  *
- * Return= The address of the new node, or NULL if it failed
+ * Return: The address of the new node, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str);
 
 /**
- * add_node_end : Adds a new node at the end of a linked list
- * @head= Pointer to the pointer to the head node of the list
- * @str= The string to be added to the new node
+ * add_node_end - Adds a new node at the end of a linked list
+ * @head: Pointer to the pointer to the head node of the list
+ * @str: The string to be added to the new node
  *
- * Return= The address of the new node, or NULL if it failed
+ * Return: The address of the new node, or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str);
 
 /**
- * free_list : Frees a linked list
- * @head= Pointer to the head node of the list
+ * free_list -  Frees a linked list
+ * @head: Pointer to the head node of the list
  */
 void free_list(list_t *head);
 
