12af3eec0a8ca8db265e9e135d9a3ea0d8043f56
hinamuriayu13 <harukanyan1@gmail.com>
2023-06-01 12:11:20 -0700

    task2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,12 +14,10 @@ list_t *add_node(list_t **head, const char *str)
     if (str == NULL)
         return NULL;
 
-    // Allocate memory for the new node
     list_t *new_node = malloc(sizeof(list_t));
     if (new_node == NULL)
         return NULL;
 
-    // Duplicate the string using strdup
     new_node->str = strdup(str);
     if (new_node->str == NULL)
     {
@@ -27,13 +25,9 @@ list_t *add_node(list_t **head, const char *str)
         return NULL;
     }
 
-    // Set the length of the string
-    new_node->len = strlen(str);
-
-    // Update the next pointer to point to the previous head
+    new_node->len = strlen(new_node->str);
     new_node->next = *head;
 
-    // Update the head to point to the new node
     *head = new_node;
 
     return new_node;
